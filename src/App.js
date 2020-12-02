911f7a1f302d1e60e99950e7de01f606a9e7ede2
DujeV <dvidov01@fesb.hr>
2020-12-02 23:03:11 +0100

    adding utils

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
 
+//Import util
+import data from './util';
+
 function App() {
   return (
     <div className='App'>
